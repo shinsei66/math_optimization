504f74264cfc74e503f7899c963ee81baa6cec73
shinsei66 <48095788+shinsei66@users.noreply.github.com>
2020-10-25 01:30:47 +0900

    整数最適化問題：鶴亀蛸算

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # Installation
 `conda install -c gurobi gurobi`
+
 Liscence is required for gurobi
 
 `pip install pulp`
--- linear_opt_plp.py
+++ linear_opt_plp.py
@@ -1,5 +1,6 @@
 import time
 import pulp
+#線形最適化問題
 sts = time.time()
 
 problem = pulp.LpProblem('lo1', pulp.LpMaximize )
@@ -15,3 +16,20 @@ ed = time.time()
 print('Optimization calculation took {:.2f} sec'.format(ed-sts))
 print('Opt. Val is {}'.format(problem.objective.value()))
 print("x1 = {}, x2 = {}, x3 = {} ".format(x1.value(), x2.value(), x3.value()) )
+
+#整数最適化問題
+sts = time.time()
+
+problem = pulp.LpProblem('lio2', pulp.LpMinimize )
+x = pulp.LpVariable('x',lowBound= 0, cat='Integer')
+y = pulp.LpVariable('y', lowBound=0, cat='Integer')
+z = pulp.LpVariable('z',lowBound= 0 ,cat='Integer')
+problem += y + z
+problem += x + y + z == 32
+problem += 2*x + 4*y + 8*z == 80
+status = problem.solve()
+ed = time.time()
+
+print('Optimization calculation took {:.2f} sec'.format(ed-sts))
+print('Opt. Val is {}'.format(problem.objective.value()))
+print("Crane = {}, Turtle = {}, Octopus = {} ".format(x.value(), y.value(), z.value()) )
